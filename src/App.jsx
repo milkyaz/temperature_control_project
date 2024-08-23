84119736c2a70a5e3798fec9b41492353182074b
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-08-23 22:19:51 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import React, { useState } from "react";
 
 function App() {
   const [temperatureValue, setTemperatureValue] = useState(10);
+  const [color, setColor] = useState("cold");
   const increase = () => {
-    setTemperatureValue(temperatureValue + 1);
+    const temperature = temperatureValue + 1;
+    if (temperature === 31) return;
+    if (temperature >= 15) {
+      setColor("hot");
+    }
+
+    setTemperatureValue(temperature);
   };
   const decrease = () => {
-    setTemperatureValue(temperatureValue - 1);
+    const temperature = temperatureValue - 1;
+    if (temperature === -31) return;
+    if (temperature <= 15) {
+      setColor("cold");
+    }
+    setTemperatureValue(temperature);
   };
   return (
     <div className="app-container ">
       <div className="temperature-display-container">
-        <div className="temperature-display ">{temperatureValue}&deg;C</div>
+        <div className={`temperature-display ${color}`}>
+          {temperatureValue}&deg;C
+        </div>
       </div>
       <div className="button-container">
         <button onClick={() => increase()}>+</button>
